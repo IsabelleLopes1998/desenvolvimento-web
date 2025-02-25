0bda6209f8ed8a0ca4596c08360892ea7c3d3378
Isabelle Lopes <isabelle.biomed@gmail.com>
2025-02-25 14:56:42 -0300

    fist commit

---
+++ src/main/java/com/br/demo/repository/ProdutoRepository.java
@@ -0,0 +1,23 @@
+package com.br.demo.repository;
+
+import com.br.demo.model.Produto;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProdutoRepository {
+    private static List<Produto> produtos = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public List<Produto> findAll(){
+        return produtos;
+    }
+    public Optional<Produto> findByID(Long id){
+        return produtos.stream().filter(p -> p.getId().equals(id)).findFirst();
+    }
+
+
+}
